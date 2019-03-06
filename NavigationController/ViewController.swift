802a9dfdbd6c8dcd66586197698bafb26859bf52
Vladislav <vladislav14141@yandex.ru>
2019-03-06 15:40:07 +1000

    Initial Commit

---
+++ NavigationController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationController
+//
+//  Created by Миронов Влад on 06/03/2019.
+//  Copyright © 2019 Миронов Влад. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
